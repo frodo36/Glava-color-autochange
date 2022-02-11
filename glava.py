b900fb082fe1555d08403d760e9d3736d149aa42
frodo36 <88240067+frodo36@users.noreply.github.com>
2022-02-11 15:36:02 +0100

    Update glava.py

--- glava.py
+++ glava.py
@@ -1,11 +1,29 @@
+from __future__ import print_function
 from subprocess import Popen
 from time import sleep
 import psutil
 from color_palette import ColorPalette
 from string import digits, ascii_lowercase
 from subprocess import check_output
+import binascii
+from PIL import Image
+import numpy as np
+import scipy
+import scipy.misc
+import scipy.cluster
+from os import getlogin
+from os import path
 
 chars = digits + ascii_lowercase + "#"
+
+if not path.exists(f"/home/{getlogin()}/.config/glava/bars.glsl"):
+    print("Glava config file not found, glava not installed")
+    input("Press any key to install...")
+    Popen("git clone https://github.com/jarcode-foss/glava")
+    Popen("cd glava")
+    Popen("meson build --prefix /usr")
+    Popen("ninja -C build")
+    Popen("sudo ninja -C build install")
 
 def process():
     PROCNAME = "glava"
@@ -13,10 +31,30 @@ def process():
         if proc.name() == PROCNAME:
             proc.kill()
 
+def get_hex(file):
+    NUM_CLUSTERS = 5
+    im = Image.open(file)
+    im = im.resize((150, 150))      # optional, to reduce time
+    ar = np.asarray(im)
+    shape = ar.shape
+    ar = ar.reshape(scipy.product(shape[:2]), shape[2]).astype(float)
+    codes, dist = scipy.cluster.vq.kmeans(ar, NUM_CLUSTERS)
+    vecs, dist = scipy.cluster.vq.vq(ar, codes)         # assign codes
+    counts, bins = scipy.histogram(vecs, len(codes))    # count occurrences
+    index_max = scipy.argmax(counts)                    # find most frequent
+    peak = codes[index_max]
+    colour = binascii.hexlify(bytearray(int(c) for c in peak)).decode('ascii')
+    hex = "#" + colour
+    print(hex)
+    return hex
+
 def start(prev_file):
     print("starting")
     while True:
-        file = list(check_output(["gsettings get org.gnome.desktop.background picture-uri"], shell=True).decode())
+        try:
+            file = list(check_output(["gsettings get org.gnome.desktop.background picture-uri"], shell=True).decode())
+        except:
+            start(prev_file)
         file = file[8:-2]
         str_file = ""
         for x in file:
@@ -27,23 +65,18 @@ def start(prev_file):
             print("wallpaper changed")
             print(file)
             print(prev_file)
+            print("trying to find hex")
             try:
-                if not prev_file == file:
-                    print("restarting")
-                    start(prev_file)
-                else:
-                    print("trying to find hex")
-                    hex_color = str((ColorPalette(file, 1, 'hex')))
+                hex_color = get_hex(file)
             except:
-                print("failed, retrying")
-                start(prev_file)
+                hex_color = get_hex(file)
             print("succeed")
             final_hex_color = ""
             for x in hex_color:
                 if x in chars:
                     final_hex_color += x
             print(final_hex_color)
-            glava_config_file = "/home/merlijn/.config/glava/bars.glsl"
+            glava_config_file = f"/home/{getlogin()}/.config/glava/bars.glsl"
             with open(glava_config_file, 'r') as glava_file:
                 data = glava_file.readlines()
             data[22] = f"#define COLOR ({final_hex_color} * GRADIENT)\n"
